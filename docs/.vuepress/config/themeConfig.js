62c9db56c72ffc13e087ae13bddf50bbf28ad7c6
vicxsl <vicxsl@163.com>
2021-05-27 01:23:59 +0800

    修改主题配置

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/EB-logo.png', // 导航栏logo
-  repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+  repo: 'vicxsl/vicxsl.github.io', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
@@ -47,8 +47,8 @@ module.exports = {
 
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: 'xugaoyi', // 必需
-    link: 'https://github.com/xugaoyi', // 可选的
+    name: 'xushengli', // 必需
+    link: 'https://github.com/vicxsl', // 可选的
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
@@ -73,7 +73,7 @@ module.exports = {
       {
         iconClass: 'icon-erji',
         title: '听音乐',
-        link: 'https://music.163.com/#/playlist?id=755597173',
+        link: 'http://music.163.com/playlist?id=2424753600',
       },
     ],
   },
@@ -81,7 +81,7 @@ module.exports = {
     // 页脚信息
     createYear: 2019, // 博客创建年份
     copyrightInfo:
-      'Vic Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
+      'Vic | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
   },
   htmlModules // 插入hmtl(广告)模块
 }
